14fd8ae955c8ce31bdec24a67bb9d7a75ebef1e3
Griffin <you@example.com>
2022-12-10 11:04:37 +0000

    day 9

--- src/day9.py
+++ src/day9.py
@@ -1,86 +1,108 @@
 import numpy as np
-import math
+from copy import deepcopy
 
 with open('../data/example.txt', 'r') as f:
     lines = [line for line in f]
 
 class Rope:
     
-    def __init__(self, shape):
+    def __init__(self, shape=(12,12), len=2):
         self.shape = shape
+        self.num_knots = len
         self.grid = np.full(self.shape, '.')
         self.visited = np.zeros(self.shape, dtype=int)
-        self.head = (int(self.shape[0]/2),int(self.shape[1]/2))
-        self.tail = self.head
-        # self.grid = np.full((5, 6), '.')
-        # self.update_grid()
+        self.start = (int(self.shape[0]/2),int(self.shape[1]/2))
+        self.knots = [self.start for i in range(self.num_knots)]
         self.mark_tail()
-    
-    # def update_grid(self):
-    #     self.grid = np.full((5, 6), '.')
-    #     self.grid[self.tail] = 'T'
-    #     self.grid[self.head] = 'H'
-        
+
     def mark_tail(self):
-        self.visited[self.tail] = 1
+        self.visited[self.knots[-1]] = 1
         
     def show(self):
         ANSI_COLOR = '\033[92m'
         ANSI_END = '\033[0m'
         self.grid = np.full(self.shape, '.')
-        self.grid[self.tail] = 'T'
-        self.grid[self.head] = 'H'
-        for i in range(len(self.grid)):
+        self.grid[self.start] = 's'
+        for i in reversed(range(1, self.num_knots)):
+            self.grid[self.knots[i]] = i
+        self.grid[self.knots[0]] = 'H'
+        for i in range(self.shape[0]):
             print(' '.join(self.grid[i]), end='    ')
             print(' '.join([f'{v}' if v == 0 else f'{ANSI_COLOR}{v}{ANSI_END}' for v in self.visited[i]]))
         print()
+
     def move_rope(self, operation, verbose=0):
         direction, magnitude = operation.replace('\n', '').split(' ')
         
+        # rope moves in iterations (4 individual steps, not 4 steps at once)
         for i in range(int(magnitude)):
-            # copy current head position before moving it (to update tail later if needed)
-            old_head = self.head
+
+            # # copy current knot positions before moving rope (to update trailing positions later if needed)
+            # knots_copy = deepcopy(self.knots)
+            # prev_knot = self.knots[0]
+
+            # move first knot
             # move up
             if direction == 'U':
-                self.head = (self.head[0] - 1, self.head[1])
+                self.knots[0] = (self.knots[0][0] - 1, self.knots[0][1])
             # move down
             elif direction == 'D':
-                self.head = (self.head[0] + 1, self.head[1])
+                self.knots[0] = (self.knots[0][0] + 1, self.knots[0][1])
             # move right
             elif direction == 'R':
-                self.head = (self.head[0], self.head[1] + 1)
+                self.knots[0] = (self.knots[0][0], self.knots[0][1] + 1)
             # move left
             elif direction == 'L':
-                self.head = (self.head[0], self.head[1] - 1)
+                self.knots[0] = (self.knots[0][0], self.knots[0][1] - 1)
+            if verbose == 3:
+                print('--------------------------')
+                print(operation, end='')
+                self.show()
+            for k in range(1, self.num_knots):
+                d = self.dist_to_prev(k)
+                prev_knot = self.knots[k-1]
+                cur_knot = self.knots[k]
+                if d == 5:
+                    new_row = cur_knot[0] + (1 if prev_knot[0] > cur_knot[0] else -1)
+                    new_col = cur_knot[1] + (1 if prev_knot[1] > cur_knot[1] else -1)
+                    self.knots[k] = (new_row, new_col)
+
+                elif self.dist_to_prev(k) > 2:
+                    new_row = cur_knot[0] + int((prev_knot[0] - cur_knot[0])/2)
+                    new_col = cur_knot[1] + int((prev_knot[1] - cur_knot[1])/2)
+                    self.knots[k] = (new_row, new_col)
+                    # prev_knot = self.knots[k]
+                if verbose == 3:
+                    print(d)
+                    # prev_knot = self.knots[k-1]
+            
+            self.mark_tail()
+
             if verbose==2:
                 print('--------------------------')
+                print(operation, end='')
                 self.show()
-            
-            # if rope is not touching, move the tail to where the head used to be
-            if self.plank_dist() > 2:
-                self.tail = old_head
-                    
-            self.mark_tail()
-            if verbose==2:
-                self.show()
-                print()
                 
         if verbose==1:
             print('--------------------------')
             print(operation, end='')
             self.show()
             
-    def plank_dist(self):
-        return int(math.dist(self.head, self.tail)**2)
+    def dist_to_prev(self, i):
+        return int(np.linalg.norm(np.asarray(self.knots[i-1])-np.asarray(self.knots[i]))**2)
+        # return int(math.dist(self.knots[i-1], self.knots[i])**2)
             
     def count_visited(self):
         return np.sum(self.visited)
     
         
-r = Rope(shape=(1000, 1000))
-# r.show()
+r = Rope(shape=(500, 500), len=2)
 with open('../data/day9.txt', 'r') as f:
-# with open('../data/example.txt', 'r') as f:
     for line in f:
         r.move_rope(line, verbose=0)
-print(r.count_visited())
+print(f'\nPart 1\n{r.count_visited()}')
+r2 = Rope(shape=(500, 500), len=10)
+with open('../data/day9.txt', 'r') as f:
+    for line in f:
+        r2.move_rope(line, verbose=0)
+print(f'\nPart 2\n{r2.count_visited()}\n')
