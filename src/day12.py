41a81376ec1095b4816f58dd171a8914a0c6623d
Griffin Reichert <griffin.reichert@ftidigitalscience.org>
2022-12-19 10:29:53 -0700

    day 12 complete

--- src/day12.py
+++ src/day12.py
@@ -1,7 +1,6 @@
-import copy
-import pandas as pd
 import numpy as np
-from pprint import pprint
+from string import ascii_lowercase
+from heapq import heappop,  heappush
 
 map = []
 # with open('../data/example.txt') as f:
@@ -20,121 +19,104 @@ class Grid:
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
                 # mark start and end points
-                if ord(map[i][j]) == 83:
+                if map[i][j] == 'S':
                     self.start = (i, j)
                     self.grid[i,j] = 0
-                elif ord(map[i][j]) == 69:
+                elif map[i][j] == 'E':
                     self.end = (i, j)
-                    self.grid[i,j] = 27
+                    self.grid[i,j] = 25
                 else:
-                    self.grid[i,j] = ord(map[i][j]) - 96
+                    self.grid[i,j] = ascii_lowercase.index(map[i][j])
 
         print(self.start, self.end)
-
-    @property    
-    def show(self):
-        for row in self.grid:
-            print(' '.join([f'{i:2d}' for i in row]))
 
     def shortest_path(self):
         # dijkstras shortest path algorithm
         visited = np.zeros(self.shape, dtype='int')
-        queue = [{'cost': self.grid[self.start], 'path':[self.start]}]
-        return self.shortest_path_helper(queue, visited)
+        # initialise heap
+        heap = [(0, self.start, [self.start])]
 
-    def shortest_path_helper(self, queue, visited):
-        
-
-
-        # explore
-        cur = queue.pop(0)
-        pos = cur['path'][-1]
-
-
-
-        # base case, return when you have found the end
-        if pos == self.end:
+        while True:
+            cost, pos, path = heappop(heap)
             
-            # print the current state
-            print(f'\n{pos}')
-            for i in range(self.shape[0]):
-                for j in range(self.shape[1]):
-                    p = '.'
-                    if (i, j) in cur['path']:
-                        p = '*'
-                    if pos == (i, j):
-                        p = '@'
-                    if self.grid[i][j] == 0:
-                        p = 'S'
-                    if self.grid[i][j] == 27:
-                        p = 'E'
-                    if (i, j) == self.end:
-                        p ='$'
-                    print(p, end=' ')
+            # if you have already visited this cell, skip as there is a shorter path to get to it already in the heap
+            if visited[pos] == 1:
+                continue
+            
+            # mark current cell as visited
+            visited[pos] = 1
+            
+            # base case, return when you have found the end
+            if pos == self.end:
+                # print the path
                 print()
-            return cur
+                for i in range(self.shape[0]):
+                    for j in range(self.shape[1]):
+                        p = '.'
+                        if (i, j) in path:
+                            p = '*'
+                        if (i, j) == self.start:
+                            p = 'S'
+                        if (i, j) == self.end:
+                            p ='$'
+                        print(p, end=' ')
+                    print()
+                return cost, path
 
-        # # mark current cell as visited
-        # visited[pos] = 1
+            # add adjacent cells to the stack if not greater than cur + 1, within bounds
+            for tup in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                ii = pos[0] + tup[0]
+                jj = pos[1] + tup[1]
+                if 0 <= ii < self.shape[0] and 0 <= jj < self.shape[1]:
+                    if (self.grid[pos] + 1 >= self.grid[ii, jj]):
+                        heappush(heap, (cost + 1, (ii, jj), path + [(ii, jj)]))
 
-        # add to adjacent cells to the stack if not greater than cur + 1, within bounds, and has not been visited
+    def shortest_path_pt2(self):
+        # dijkstras shortest path algorithm
+        visited = np.zeros(self.shape, dtype='int')
+        # initialise heap
+        heap = [(0, self.end, [self.end])]
 
-        # check north
-        north = (pos[0] - 1, pos[1])
-        if (north not in cur['path']) and (north[0] >= 0) and (self.grid[north] <= self.grid[pos] + 1) and (self.grid[north] >= self.grid[pos]):
-            tmp = cur['path'] + [north]
-            queue.append({
-                'cost': cur['cost'] + self.grid[north], 
-                'path': tmp
-            })
-        
-        # check south
-        south = (pos[0] + 1, pos[1])
-        # if pos == (4,2):
-        #     print('hit it')
-        #     print(cur)
-        #     print(south)
-        #     print(self.grid.shape)
-        if (south not in cur['path']) and (south[0] < self.shape[0]) and (self.grid[south] <= self.grid[pos] + 1) and (self.grid[south] >= self.grid[pos]):
-            tmp = cur['path'] + [south]
-            queue.append({
-                'cost': cur['cost'] + self.grid[south], 
-                'path': tmp
-            })
+        while True:
+            cost, pos, path = heappop(heap)
+            
+            # if you have already visited this cell, skip as there is a shorter path to get to it already in the heap
+            if visited[pos] == 1:
+                continue
+            
+            # mark current cell as visited
+            visited[pos] = 1
+            
+            # base case, return when you have found a cell at elevation 0
+            if self.grid[pos] == 0:
+                # print the path
+                print()
+                for i in range(self.shape[0]):
+                    for j in range(self.shape[1]):
+                        p = '.'
+                        if (i, j) in path:
+                            p = '*'
+                        if (i, j) == self.end:
+                            p = 'S'
+                        if (i, j) == pos:
+                            p = 'a'
+                        print(p, end=' ')
+                    print()
+                return cost, path
 
-        # check east
-        east = (pos[0], pos[1] + 1)
-        if (east not in cur['path']) and (east[1] < self.shape[1]) and (self.grid[east] <= self.grid[pos] + 1) and (self.grid[east] >= self.grid[pos]):
-            tmp = cur['path'] + [east]
-            queue.append({
-                'cost': cur['cost'] + self.grid[east], 
-                'path': tmp
-            })
-
-        # check west
-        west = (pos[0], pos[1] - 1)
-        if (west not in cur['path']) and (west[1] >= 0) and (self.grid[west] <= self.grid[pos] + 1) and (self.grid[west] >= self.grid[pos]):
-            tmp = cur['path'] + [west]
-            queue.append({
-                'cost': cur['cost'] + self.grid[west], 
-                'path': tmp
-            })
-        
-        # {k: v for k, v in sorted(elves.items(), key=lambda item: -item[1])}
-        queue = sorted(queue, key=lambda x: x['cost'])
-        # print(f'{queue}')
-        return self.shortest_path_helper(queue, visited)
-
-        
-        # sort the stack in ascending order
-        # move to the cell on the top of the stack
-        # add that cells adjacent cells to the stack, sort the stack
-
+            # add adjacent cells to the stack if not less than cur - 1, within bounds
+            for tup in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                ii = pos[0] + tup[0]
+                jj = pos[1] + tup[1]
+                if 0 <= ii < self.shape[0] and 0 <= jj < self.shape[1]:
+                    if (self.grid[pos] - 1 <= self.grid[ii, jj]):
+                        heappush(heap, (cost + 1, (ii, jj), path + [(ii, jj)]))
 
 g = Grid(map)
-g.show
 
-res = g.shortest_path()
+path_len, path = g.shortest_path()
 
-print(res)
-print(len(res['path']) - 1)
+path_len_2, path_2 = g.shortest_path_pt2()
+
+print(f'\nPart 1\n{path_len}')
+print(f'\nPart 2\n{path_len_2}')
