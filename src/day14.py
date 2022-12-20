ccaa6f2edc77af01cff2f5b6a94b71fd16abbdc2
Griffin Reichert <griffin.reichert@ftidigitalscience.org>
2022-12-20 08:11:20 -0700

    day 14 complete

--- src/day14.py
+++ src/day14.py
@@ -1,7 +1,6 @@
 from pprint import pprint
 
-with open('../data/example.txt') as f:
-# with open('../data/day14.txt') as f:
+with open('../data/day14.txt') as f:
     lines = f.read().strip().split('\n')
     
 rocks = [[eval(coord) for coord in l.split('->')] for l in lines]
@@ -9,20 +8,18 @@ rocks = [[eval(coord) for coord in l.split('->')] for l in lines]
 class Cave:
     def __init__(self, rocks):
         max_depth = 0
-        # max_left = 0
-        # max_right = 1000
         for r in rocks:
             for tup in r:
-                if tup[0] < max_left:
-                    max_left = tup[0]
-                if tup[0] > max_right:
-                    max_right = tup[0]
                 if tup[1] > max_depth:
                     max_depth = tup[1]
                     
-        self.shift = max_left - 1
-        self.cave = [['.']*(2*max_depth + 3) for _ in range(max_depth+2)]
-        self.cave.append(['#']*(2*max_depth + 3))
+        max_left = 500 - 2*max_depth
+        max_right = 500 + 2*max_depth
+        row_width = max_right - max_left
+                    
+        self.shift = max_left + 1
+        self.cave = [['.']*(row_width) for _ in range(max_depth+2)]
+        self.cave.append(['#']*(row_width))
         self.drop_point = 500 - self.shift
         self.cave[0][self.drop_point] = '+'
         self.n = len(self.cave)
@@ -37,7 +34,6 @@ class Cave:
                 cur_j = rock[r][0] - self.shift
                 nxt_i = rock[r+1][1] 
                 nxt_j = rock[r+1][0] - self.shift
-                # print(cur_i, nxt_i, cur_j, nxt_j)
                 for i in range(min(cur_i, nxt_i), max(cur_i, nxt_i) + 1):
                     self.cave[i][cur_j] = '#'
                 for j in range(min(cur_j, nxt_j), max(cur_j, nxt_j) + 1):
@@ -45,10 +41,11 @@ class Cave:
     
     def fill_sand(self):
         while True:
-            # drop a grain of sand
+            # drop a grain of sand starting at the drop point
             i = 0
             j = self.drop_point
             while True:
+                # if sand hits the row of air above the floor, return the number of grains of sand that it took
                 if i+1 == self.n - 1:
                     return self.grains
                 # if space below is air, move sand down one
@@ -66,20 +63,50 @@ class Cave:
                     i += 1
                     j += 1
                     continue
+                # if none of those conditions are true, then you are no longer falling
+                break
+            self.cave[i][j] = 'o'
+            self.grains +=1
+    
+    def fill_sand_pt2(self):
+        while True:
+            # drop a grain of sand
+            i = 0
+            j = self.drop_point
+            while True:
+                if self.cave[0][self.drop_point] == 'o':
+                    return self.grains
+                # if space below is air, move sand down one
+                if self.cave[i+1][j] == '.':
+                    i += 1
+                    continue
+                # otherwise the space below is blocked
+                # if left is not blocked
+                if self.cave[i+1][j-1] == '.':
+                    i += 1
+                    j -= 1
+                    continue
+                # if right is air
+                if self.cave[i+1][j+1] == '.':
+                    i += 1
+                    j += 1
+                    continue
+                # if none of those conditions are true, then you are no longer falling
                 break
             self.cave[i][j] = 'o'
             self.grains +=1
 
-
-
     def show(self):
         print()
         for row in self.cave:
-            print(' '.join(row))        
-
+            print(''.join(row))  
+        print()      
 
 c = Cave(rocks)
 c.show()
 grains = c.fill_sand()
+c.show()
+print(grains)
+grains = c.fill_sand_pt2()
 c.show()
 print(grains)
