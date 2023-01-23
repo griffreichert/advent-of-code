76c2d517e3856e28bb6222ce9d6f385d1768bb90
Griffin <you@example.com>
2023-01-23 23:47:55 +0000

    day19 complete

--- src/day19.py
+++ src/day19.py
@@ -1,6 +1,8 @@
 import re
 from functools import cache
 from collections import deque
+import time
+
 with open('../data/day19.txt', 'r') as f:
     lines = f.readlines()
     
@@ -16,8 +18,15 @@ for line in lines:
         (nums[2], 0, 0, 0), # Clay Robot
         (nums[1], 0, 0, 0), # Ore Robot
     ))
-    
-print(blueprints[0])
+
+test_blueprint = (
+    (2, 0, 7, 0),
+    (3, 14, 0, 0),
+    (2, 0, 0, 0),
+    (4, 0, 0, 0),
+)    
+
+# print(blueprints[0])
     
 def get_max_costs(blueprint):
     return tuple(max(a, b, c, d) for a, b, c, d in zip(*blueprint))
@@ -28,80 +37,83 @@ def collect_geodes(blueprint, robots=(1,0,0,0), resources=(0,0,0,0), toc=24):
     maxcosts = get_max_costs(blueprint)
     # queue for bfs
     queue = deque()
-    queue.add((toc, robots, resources))
+    queue.append((toc, robots, resources))
     # set to memoize states 
     seen = set()
     best_geodes = 0
     
     while queue:
         state = queue.popleft()
+
+        # prune queue if we have seen this state before
         if state in seen:
-            return seen
+            continue
+        seen.add(state)
+
+        # if len(queue) % 10000 == 0:
+        #     print(state, len(queue))
+
         toc, robots, resources = state
-    # Return the number of geodes collected when you run out of time to make more robots
-    if toc == 1:
-        return resources[3] + robots[3]
-    
-    # Collect resources with existing robots
-    new_resources = tuple(x + y for x, y in zip(resources, robots))
-    # print('\ndont build')
-    # print(new_resources)
-    # Default to building no new robots in the current minute    
-    best_geodes = collect_geodes(blueprint, maxcosts, toc - 1, robots, new_resources)
+        
+        # if we run out of time stop searching
+        if toc == 1:
+            # update best geodes accordingly: number you have plus what you can make in the final minute
+            best_geodes = max(best_geodes, resources[3] + robots[3])
+            continue
+        
+        # Collect resources with existing robots
+        collected_resources = tuple(x + y for x, y in zip(resources, robots))
+        # Cap collected resources if the ammount you have is more than you could ever spend (this prunes queue)
+        maxspend = tuple(toc * cost if cost != 0 else 99 for cost in maxcosts)
+        collected_resources = tuple(min(x, y) for x, y in zip(maxspend, collected_resources))
+       
+        # Default to building no new robots in the current minute    
+        queue.append((toc - 1, robots, collected_resources))
 
-    # Start building a new robot if possible
-    for r, costs in enumerate(blueprint):
-        # if you can afford to build a robot, try it and add the number of geodes to the list of possible outcomes
-        # (note that you use the resources you had before you started collecting)
-        if all(x <= y for x, y in zip(costs, resources)):
-            
-            # Optimization: dont build more robots if you have more of that robot than the max cost
-            mineral_idx = 4 - (r + 1)
-            if r > 0 and robots[mineral_idx] >= maxcosts[mineral_idx]:
-                continue
-            # print('\ninvestigating', r, costs)
-            # print('resources:', resources)
-            # pay cost of new robot
-            new_resources = tuple(x - y for x, y in zip(new_resources, costs))
-            # build the new robot, increment the index in the robots tuple corresponding to the new one
-            # EX: robots=(1, 0, 2, 0), build a clay robot, new_robots=(1, 1, 2, 0)
-            new_robots = tuple(
-                x + y for x, y in zip(robots, 
-                                      tuple(reversed([1 if i == r else 0 for i in range(4)]))))
-            best_geodes = max(best_geodes, collect_geodes(blueprint, maxcosts, toc - 1, new_robots, new_resources))
-            
-            # BE GREEDY: if you can afford to build a geode robot, always do it, skip building other robots
-            if r == 0:
-                return best_geodes
-            
+        # Start building a new robot if possible
+        for r, costs in enumerate(blueprint):
+            # if you can afford to build a robot, try it and add the number of geodes to the list of possible outcomes
+            # (note that you use the resources you had before you started collecting)
+            if all(x <= y for x, y in zip(costs, resources)):
+                
+                # Optimization: dont build more robots if you have more of that robot than the max cost
+                mineral_idx = 4 - (r + 1)
+                if r > 0 and robots[mineral_idx] >= maxcosts[mineral_idx]:
+                    continue
+
+                # pay cost of new robot
+                resources_post_cost = tuple(x - y for x, y in zip(collected_resources, costs))
+                # build the new robot, increment the index in the robots tuple corresponding to the new one
+                # EX: robots=(1, 0, 2, 0), build a clay robot, new_robots=(1, 1, 2, 0)
+                assert all(res >= 0 for res in resources_post_cost), (state, costs, resources_post_cost)
+                new_robots = tuple(
+                    x + y for x, y in zip(robots, 
+                                        tuple(reversed([1 if i == r else 0 for i in range(4)]))))
+                queue.append((toc - 1, new_robots, resources_post_cost))
+                
+                # # BE GREEDY: if you can afford to build a geode robot, always do it, skip building other robots
+                if r == 0:
+                    break
     return best_geodes
-    
-test_cases = (
-    ((1, 4, 2, 2), (5, 37, 6, 7)),  # minute 24
-    ((1, 4, 2, 2), (4, 33, 4, 5)),
-    ((1, 4, 2, 2), (3, 29, 2, 3)),
-    ((1, 4, 2, 1), (3, 29, 2, 3)),
-)
-print('test case 1')
-g = collect_geodes(blueprints[0], get_max_costs(blueprints[0]), 1, (1, 4, 2, 2), (5, 37, 6, 7))
-assert g == 9, f'!!! failed ({g})'
-print('test case 2')
-g = collect_geodes(blueprints[0], get_max_costs(blueprints[0]), 2, (1, 4, 2, 2), (4, 33, 4, 5))
-assert g == 9, f'!!! failed ({g})'
-print('test case 3')
-g = collect_geodes(blueprints[0], get_max_costs(blueprints[0]), 3, (1, 4, 2, 2), (3, 29, 2, 3))
-assert g == 9, g
 
-# print('test case 4')
-# print('test case 5')
-# print('test case 6')
-# print('test case 7')
-g = collect_geodes(blueprints[0], get_max_costs(blueprints[0]), 24)
-print(g)
+total = 0
+for b, blueprint in enumerate(blueprints):
+    # print(b+1)
+    total += (b + 1) * (collect_geodes(blueprint))
 
-# # Recursive implementation that was too slow
+print('Part 1:', total)
+
+total = 1
+for b, blueprint in enumerate(blueprints[:3]):
+    # print(b+1)
+    g = collect_geodes(blueprint, toc=32)
+    total *= g
+
+print('Part 2:', total)
+
+# # Recursive implementation that was  slower than stack based
 # @cache
-# def collect_geodes(blueprint, maxcosts, toc, robots=(1,0,0,0), resources=(0,0,0,0)):
+# def collect_geodes_rec(blueprint, maxcosts, toc=24, robots=(1,0,0,0), resources=(0,0,0,0)):
 #     """Dynamic programming algorithm to collect the most geodes in the time constraint
     
 #     Parameters:
@@ -113,72 +125,46 @@ print(g)
 #         - resources: tuple representing the number of each type of resource you have collected
 #     """
 #     print(robots, resources)
-#     # print(resources)
 #     # Base case: return the number of geodes collected when you run out of time
 #     if toc == 1:
-#         # print('\ncollected', resources[3] + robots[3], 'geodes')
-#         # print(robots)
 #         return resources[3] + robots[3]
     
 #     # Collect resources with existing robots
 #     new_resources = tuple(x + y for x, y in zip(resources, robots))
-#     # print('\ndont build')
-#     # print(new_resources)
 #     # Default to building no new robots in the current minute    
-#     best_geodes = collect_geodes(blueprint, maxcosts, toc - 1, robots, new_resources)
+#     best_geodes = collect_geodes_rec(blueprint, maxcosts, toc - 1, robots, new_resources)
 
 #     # Start building a new robot if possible
 #     for r, costs in enumerate(blueprint):
 #         # if you can afford to build a robot, try it and add the number of geodes to the list of possible outcomes
 #         # (note that you use the resources you had before you started collecting)
 #         if all(x <= y for x, y in zip(costs, resources)):
-            
 #             # Optimization: dont build more robots if you have more of that robot than the max cost
 #             mineral_idx = 4 - (r + 1)
 #             if r > 0 and robots[mineral_idx] >= maxcosts[mineral_idx]:
 #                 continue
-#             # print('\ninvestigating', r, costs)
-#             # print('resources:', resources)
 #             # pay cost of new robot
-#             new_resources = tuple(x - y for x, y in zip(new_resources, costs))
+#             resources_post_cost = tuple(x - y for x, y in zip(new_resources, costs))
 #             # build the new robot, increment the index in the robots tuple corresponding to the new one
 #             # EX: robots=(1, 0, 2, 0), build a clay robot, new_robots=(1, 1, 2, 0)
 #             new_robots = tuple(
 #                 x + y for x, y in zip(robots, 
 #                                       tuple(reversed([1 if i == r else 0 for i in range(4)]))))
-#             best_geodes = max(best_geodes, collect_geodes(blueprint, maxcosts, toc - 1, new_robots, new_resources))
+#             best_geodes = max(best_geodes, collect_geodes_rec(blueprint, maxcosts, toc - 1, new_robots, resources_post_cost))
             
-#             # BE GREEDY: if you can afford to build a geode robot, always do it, skip building other robots
+#             # Greedy Heuristic: if you can afford to build a geode robot, always do it, skip building other robots
 #             if r == 0:
-#                 return best_geodes
-            
+#                 break
 #     return best_geodes
-    
-# def get_max_costs(blueprint):
-#     return tuple(max(a, b, c, d) for a, b, c, d in zip(*blueprint))
 
-# test_cases = (
-#     ((1, 4, 2, 2), (5, 37, 6, 7)),  # minute 24
-#     ((1, 4, 2, 2), (4, 33, 4, 5)),
-#     ((1, 4, 2, 2), (3, 29, 2, 3)),
-#     ((1, 4, 2, 1), (3, 29, 2, 3)),
-# )
-# print('test case 1')
-# g = collect_geodes(blueprints[0], get_max_costs(blueprints[0]), 1, (1, 4, 2, 2), (5, 37, 6, 7))
-# assert g == 9, f'!!! failed ({g})'
-# print('test case 2')
-# g = collect_geodes(blueprints[0], get_max_costs(blueprints[0]), 2, (1, 4, 2, 2), (4, 33, 4, 5))
-# assert g == 9, f'!!! failed ({g})'
-# print('test case 3')
-# g = collect_geodes(blueprints[0], get_max_costs(blueprints[0]), 3, (1, 4, 2, 2), (3, 29, 2, 3))
-# assert g == 9, g
+# rec_start = time.time()
+# print(collect_geodes_rec(test_blueprint, get_max_costs(test_blueprint)))
+# rec_end = time.time()
+# rec_time = rec_end - rec_start
+# print('Rec time:', rec_time, 'seconds')
 
-# # print('test case 4')
-# # print('test case 5')
-# # print('test case 6')
-# # print('test case 7')
-# g = collect_geodes(blueprints[0], get_max_costs(blueprints[0]), 24)
-# print(g)
-
-# # print()
-# # print(get_max_costs(blueprints[0]))
+# stack_start = time.time()
+# print(collect_geodes(test_blueprint))
+# stack_end = time.time()
+# stack_time = stack_end - stack_start
+# print('Stack time:', stack_time, 'seconds')
