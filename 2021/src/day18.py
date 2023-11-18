85080a4bc6e62ee893836b6943ea67240723f157
Griffin Reichert <griffin.reichert@ftidigitalscience.org>
2023-11-18 20:34:31 +0000

    18

---
+++ 2021/src/day18.py
@@ -0,0 +1,38 @@
+import utils
+from collections import deque, defaultdict, Counter
+from heapq import heappop, heappush
+import numpy as np
+import re
+from pprint import pprint
+
+
+lines = utils.read_list(__file__, as_str=True)
+# grid = utils.lines_to_grid(lines)
+
+
+class SnailNum:
+    def __init__(self):
+        pass
+
+    def split(self):
+        pass
+
+    def explode(self):
+        pass
+
+
+def p1():
+    res = 0
+    return res
+
+
+def p2():
+    res = 0
+    return res
+
+
+_p1 = p1()
+_p2 = p2()
+
+print(f"p1\n{utils.Ansii.green}{_p1}{utils.Ansii.clear}")
+print(f"p2\n{utils.Ansii.green}{_p2}{utils.Ansii.clear}")
