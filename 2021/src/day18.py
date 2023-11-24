2617edcca2f0c6e0ed9cc333913227d1dadc91df
Griffin Reichert <griffin.reichert@ftidigitalscience.org>
2023-11-24 17:30:37 +0000

    exploding works

--- 2021/src/day18.py
+++ 2021/src/day18.py
@@ -24,165 +24,190 @@ def snailfish_magnitude(snailfish: str):
 
 
 def reduce_snailfish(snailfish):
+    def traverse_until_digit(index: int, forwards=True) -> int:
+        """start at a position and traverse left or right until you find a digit
+
+        Args:
+            index (int): starting index
+            forwards (bool, optional): whether to traverse forwards or backwards. Defaults to True.
+
+        Returns:
+            tuple (int, int): start and end indexes of the number, None if we reach the end in either direction
+        """
+        i = index
+        while not snailfish[i].isdigit():
+            if i <= 0 or i >= len(snailfish):
+                return None
+            if forwards:
+                i += 1
+            else:
+                i -= 1
+        start = i
+        end = i
+        # test for it being a double digit
+        # if going forwards, you've just reached the start digit
+        if forwards:
+            if snailfish[i + 1].isdigit():
+                end += 1
+        # if going backwards, you've just reached the last digit
+        elif not forwards:
+            if snailfish[i - 1].isdigit():
+                start -= 1
+
+        return start, end
+
+    def get_int_from_indices(i: int, j: int) -> int:
+        return int(snailfish[i : j + 1])
+
     depth = 0
     i = 0
-    while True:
-        # get all the way through without exploding
-        while i < len(snailfish):
-            # are we dealing with a digit or a symbol
-            char = snailfish[i]
-            # dont need to do anything for a comma
-            if char == ",":
-                pass
-            # increase depth
-            elif char == "[":
-                depth += 1
-            # decrease depth
-            elif char == "]":
-                depth -= 1
-            # dealing with a digit
-            else:
-                double_digit = snailfish[i + 1].isdigit()
-                # explode if depth greater than 4
-                if depth > 4:
-                    # pairs left value (if any) is added to the first number to the left
-                    # find the first value to the left
-                    l = i - 2
-                    while not snailfish[l].isdigit():
-                        l -= 1  # keep moving left
-                        if i < 0:
-                            break
-                            # todo
-                    first_left_num = snailfish[l]
-                    # bump in case it's a double digit
-                    first_left_bump = 0
-                    # check if left is a double digit number
-                    if snailfish[l - 1].isdigit():
-                        l -= 1
-                        first_left_num += int(snailfish[l]) * 10
-                        first_left_bump = 1
+    # while True:
+    # get all the way through without exploding
+    while i < len(snailfish):
+        # are we dealing with a digit or a symbol
+        char = snailfish[i]
+        # dont need to do anything for a comma
+        if char == ",":
+            pass
+        # increase depth
+        elif char == "[":
+            depth += 1
+        # decrease depth
+        elif char == "]":
+            depth -= 1
+        # dealing with a digit
+        else:
+            # j stores the end index of the current number
+            j = i
+            if snailfish[i + 1].isdigit():
+                j += 1
 
-                    # add left value from the pair to the first number on the left
-                    if double_digit:
-                        lnum = int(snailfish[i : i + 2])
-                    else:
-                        lnum = int(snailfish[i])
-
-                    first_left_num += lnum
-                    
-                    # rebuild snailfish
-                    snailfish = f'{snailfish[:l]}{lnum}{snailfish[l+first_left_bump+1:i-2]}
-
-                    # pairs right value is added to the first number to the right
-                    # the pair is replaced with the regular number 0
-                else:
-                    # dont check this number twice
-                    if double_digit:
-                        i += 1
-            # next number
-            i += 1
-
-            # print("\n", i)
-            # pprint(snailfish)
-            num, depth = snailfish[i]
-
-            # explode the leftmost pair with depth greater than 4
+            # explode if depth greater than 4
             if depth > 4:
-                # print()
-                # print("exploding", i, (num, snailfish[i + 1][0]), "with depth", depth)
-                prev_chunk = []
-                chunk = []
-                next_chunk = []
-                nnum, _ = snailfish[i + 1]
-                # pairs left value (if any) is added to the first number to the left
-                if i > 0:
-                    prev_num, prev_depth = snailfish[i - 1]
-                    prev_chunk = snailfish[: i - 1]
-                    chunk.append((prev_num + num, prev_depth))
+                print("exploding")
+                print(i, snailfish[i], depth)
+                depth -= 1
+                new_snailfish = ""
 
-                # the pair is replaced with the regular number 0
-                chunk.append((0, depth - 1))
+                # get the current number (left in the pair)
+                cur_left = get_int_from_indices(i, j)
+                #                                    *
+                #                      1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 2 2 3 3 3
+                #  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2
+                #  [ [ [ [ 0 , 7 ] , 4 ] , [ 7 , [ [ 8 , 4 ] , 9 ] ] ] , [ 1 , 1 ] ]
 
-                # pairs right value is added to the first number to the right
-                if i < len(snailfish) - 2:
-                    nxt_num, nxt_depth = snailfish[i + 2]
-                    next_chunk = snailfish[i + 3 :]
-                    chunk.append((nxt_num + nnum, nxt_depth))
+                # find the first value to the left
+                left_indices = traverse_until_digit(i - 1, forwards=False)
+                print("LL", left_indices)
+                # pairs left value is added to the first number to the left (if any)
+                if left_indices:
+                    left_start, left_end = left_indices
+                    new_left = get_int_from_indices(left_start, left_end)
+                    new_left += cur_left
+                    # reconstruct the snailfish so far
+                    # assume it has brackets to each side
+                    new_snailfish += (
+                        f"{snailfish[:left_start]}{new_left}{snailfish[left_end+1:i-1]}"
+                    )
+                else:
+                    new_snailfish += f"{snailfish[:i-1]}"
 
-                snailfish = prev_chunk + chunk + next_chunk
-                # show_snailfish(snailfish)
-                # if you did explode, check the position one back
-                if i > 0:
-                    i -= 1
-                continue
-            i += 1
+                # replace the current number with 0
+                new_snailfish += "0"
 
-        # once you make it to the end of exploding, try splitting
-        i = 0
-        while i < len(snailfish):
-            num, depth = snailfish[i]
-            # Split the leftmost number greater than or equal to 10
-            if num >= 10:
-                # calculate the L and R parts of a num, even numbers split into two,
-                # odd numbers are split 13 -> (6, 7)
-                left = num // 2
-                right = (num + 1) // 2
+                # find right number in the current pair
+                ri = j + 2
+                rj = ri
+                if snailfish[ri + 1].isdigit():
+                    rj += 1
+                cur_right = get_int_from_indices(ri, rj)
 
-                snailfish = (
-                    snailfish[:i]
-                    + [(left, depth + 1), (right, depth + 1)]
-                    + snailfish[i + 1 :]
-                )
-                # print()
-                # print("splitting", i, num)
-                # show_snailfish(snailfish)
-                # if you split, start exploding again from that position
-                break
-            i += 1
+                # add right number to the first number to the right if there is any
+                right_indices = traverse_until_digit(rj + 1, forwards=True)
+                if right_indices:
+                    right_start, right_end = right_indices
+                    new_right = get_int_from_indices(right_start, right_end)
+                    new_right += cur_right
+                    new_snailfish += f"{snailfish[rj+2:right_start]}{new_right}{snailfish[right_end+1:]}"
+                else:
+                    new_snailfish += f"{snailfish[rj+2:]}"
+                print(snailfish)
+                snailfish = new_snailfish
+                print(snailfish)
+                # move i
+                # i = right_start - 1
+            else:
+                # get ready to increment i but take into acount the number could be a double digit
+                i = j
+        # next number
+        i += 1
 
-        # if you got through splitting without finding something to split, youre done
-        else:
-            return snailfish
+        # # once you make it to the end of exploding, try splitting
+        # i = 0
+        # while i < len(snailfish):
+        #     num, depth = snailfish[i]
+        #     # Split the leftmost number greater than or equal to 10
+        #     if num >= 10:
+        #         # calculate the L and R parts of a num, even numbers split into two,
+        #         # odd numbers are split 13 -> (6, 7)
+        #         left = num // 2
+        #         right = (num + 1) // 2
 
+        #         snailfish = (
+        #             snailfish[:i]
+        #             + [(left, depth + 1), (right, depth + 1)]
+        #             + snailfish[i + 1 :]
+        #         )
+        #         # print()
+        #         # print("splitting", i, num)
+        #         # show_snailfish(snailfish)
+        #         # if you split, start exploding again from that position
+        #         break
+        #     i += 1
 
-def combine_snailfish(a, b):
-    return [(a_num, a_depth + 1) for a_num, a_depth in a] + [
-        (b_num, b_depth + 1) for b_num, b_depth in b
-    ]
-
-
-snailfish = [parse_snail_num(s) for s in snailfish]
-
-# p1 - add all snailfish numbers and find the largest magnitude
-final_snailfish = snailfish[0]
-for s in snailfish[1:]:
-    final_snailfish = reduce_snailfish(combine_snailfish(final_snailfish, s))
-
-_p1 = snailfish_magnitude(final_snailfish)
-print("p1")
-# p2 - find largest magnitude from adding any of two different snailfish numbers in list
-# bruteus forceus - iterate over all combos (except the same)
-magnitudes = []
-for i, si in enumerate(snailfish):
-    for j, sj in enumerate(snailfish):
-        if i == j:
-            continue
-        else:
-            mag = snailfish_magnitude(reduce_snailfish(combine_snailfish(si, sj)))
-            magnitudes.append((mag, i, j))
-
-print(max(magnitudes, key=lambda x: x[0]))
-
-
-def p2():
-    res = 0
-    return res
+        # # if you got through splitting without finding something to split, youre done
+        # else:
+    return snailfish
 
 
-_p2 = p2()
+# def combine_snailfish(a, b):
+#     return [(a_num, a_depth + 1) for a_num, a_depth in a] + [
+#         (b_num, b_depth + 1) for b_num, b_depth in b
+#     ]
 
-print(f"p1\n{utils.Ansii.green}{_p1}{utils.Ansii.clear}")
-print(f"p2\n{utils.Ansii.green}{_p2}{utils.Ansii.clear}")
+
+# snailfish = [parse_snail_num(s) for s in snailfish]
+
+# # p1 - add all snailfish numbers and find the largest magnitude
+# final_snailfish = snailfish[0]
+# for s in snailfish[1:]:
+#     final_snailfish = reduce_snailfish(combine_snailfish(final_snailfish, s))
+
+# _p1 = snailfish_magnitude(final_snailfish)
+# print("p1")
+# # p2 - find largest magnitude from adding any of two different snailfish numbers in list
+# # bruteus forceus - iterate over all combos (except the same)
+# magnitudes = []
+# for i, si in enumerate(snailfish):
+#     for j, sj in enumerate(snailfish):
+#         if i == j:
+#             continue
+#         else:
+#             mag = snailfish_magnitude(reduce_snailfish(combine_snailfish(si, sj)))
+#             magnitudes.append((mag, i, j))
+
+# print(max(magnitudes, key=lambda x: x[0]))
+
+
+# def p2():
+#     res = 0
+#     return res
+
+
+# _p2 = p2()
+
+# print(f"p1\n{utils.Ansii.green}{_p1}{utils.Ansii.clear}")
+# print(f"p2\n{utils.Ansii.green}{_p2}{utils.Ansii.clear}")
 
 
 # def parse_snail_num(s):
