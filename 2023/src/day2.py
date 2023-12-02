3ddc07ff47c8d111f9ff299c11dce150e9ba6610
Griffin <you@example.com>
2023-12-02 05:36:54 +0000

    day2

---
+++ 2023/src/day2.py
@@ -0,0 +1,51 @@
+import utils
+from collections import defaultdict
+
+
+lines = utils.read_list(__file__, as_str=True)
+
+
+def p1():
+    # only 12 red cubes, 13 green cubes, and 14 blue cubes possible
+    res = 0
+    for i, line in enumerate(lines):
+        line = line[line.find(":") + 1 :].strip()
+        for game in line.split(";"):
+            colors = [
+                tuple(colstr.strip().split(" ")) for colstr in game.strip().split(",")
+            ]
+            colors = {b: int(a) for a, b in colors}
+            if not all(
+                colors.get(col, 0) <= colmax
+                for col, colmax in [("red", 12), ("green", 13), ("blue", 14)]
+            ):
+                break
+        else:
+            res += i + 1
+    return res
+
+
+def p2():
+    res = 0
+    for line in lines:
+        line = line[line.find(":") + 1 :].strip()
+
+        colors_needed = defaultdict(int)
+
+        for game in line.split(";"):
+            colors = [
+                tuple(colstr.strip().split(" ")) for colstr in game.strip().split(",")
+            ]
+            colors = {b: int(a) for a, b in colors}
+            for col, draws in colors.items():
+                colors_needed[col] = max(colors_needed[col], draws)
+
+        res += utils.list_product(colors_needed.values())
+    return res
+
+
+_p1 = p1()
+_p2 = p2()
+
+print(f"p1\n{utils.Ansii.green}{_p1}{utils.Ansii.clear}")
+print(f"p2\n{utils.Ansii.green}{_p2}{utils.Ansii.clear}")
