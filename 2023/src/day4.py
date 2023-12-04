68dbed78a8df0d194a19c9ae3a5caf16ee9b978d
Griffin Reichert <griffin.reichert@ftidigitalscience.org>
2023-12-04 11:30:47 +0000

    day4 complete

--- 2023/src/day4.py
+++ 2023/src/day4.py
@@ -7,17 +7,29 @@ from pprint import pprint
 
 
 lines = utils.read_list(__file__, as_str=True)
-# grid = utils.lines_to_grid(lines)
+
+
+cards = {
+    utils.find_int(line.split(":")[0]): tuple(
+        utils.find_int(part, all=True) for part in line.split(":")[-1].split("|")
+    )
+    for line in lines
+}
 
 
 def p1():
-    res = 0
-    return res
+    num_winners = [sum(n in winners for n in nums) for nums, winners in cards.values()]
+    return sum(2 ** (nw - 1) for nw in num_winners if nw > 0)
 
 
 def p2():
-    res = 0
-    return res
+    card_counts = {i + 1: 1 for i in range(len(cards))}
+    for i, (nums, winners) in cards.items():
+        num_winners = sum(n in winners for n in nums)
+        if num_winners > 0:
+            for j in range(i + 1, min(len(cards), i + num_winners) + 1):
+                card_counts[j] += card_counts[i]
+    return sum(card_counts.values())
 
 
 _p1 = p1()
---
+++ 2023/src/day5.py
@@ -0,0 +1,27 @@
+import utils
+from collections import deque, defaultdict, Counter
+from heapq import heappop, heappush
+import numpy as np
+import re
+from pprint import pprint
+
+
+lines = utils.read_list(__file__, as_str=True)
+# grid = utils.lines_to_grid(lines)
+
+
+def p1():
+    res = 0
+    return res
+
+
+def p2():
+    res = 0
+    return res
+
+
+_p1 = p1()
+_p2 = p2()
+
+print(f"p1\n{utils.Ansii.green}{_p1}{utils.Ansii.clear}")
+print(f"p2\n{utils.Ansii.green}{_p2}{utils.Ansii.clear}")
