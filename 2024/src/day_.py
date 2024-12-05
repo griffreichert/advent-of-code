b465a37968e39915677e90236a6b2e3b071fd316
Griffin Reichert <griffinreichert@gmail.com>
2024-12-05 14:04:07 +0000

    init 2024

---
+++ 2024/src/day_.py
@@ -0,0 +1,26 @@
+import itertools
+import math
+import re
+from collections import Counter, defaultdict, deque
+from heapq import heappop, heappush
+from pprint import pprint
+
+import numpy as np
+import utils
+
+lines = utils.read_lines(__file__, parse_ints=False)
+# grid = utils.lines_to_grid(lines)
+
+
+def p1():
+    res = 0
+    return res
+
+
+def p2():
+    res = 0
+    return res
+
+
+print(f"p1\n{utils.Ansii.green}{p1()}{utils.Ansii.clear}")
+print(f"p2\n{utils.Ansii.green}{p2()}{utils.Ansii.clear}")
